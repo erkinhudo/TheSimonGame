733af8051ab0c93aa3e43c775c95c780816f2747
Eric Hudo <erkinhudo@gmail.com>
2021-03-16 00:47:20 -0400

    Randomly number was created

---
+++ game.js
@@ -0,0 +1,10 @@
+
+var gamePattern = [];
+var buttonColours = ["red","blue","green","yellow"];
+var randomChosenColour = buttonColours[randomNumber];
+// kalan yerimiz , Step-2 CReat new pattern
+
+function nextSequence(){
+    var rundomNumber = Math.floor(Math.random() * 4);
+    return num;
+}
